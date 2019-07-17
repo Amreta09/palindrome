b828dc32d840e9ef85b3094580c6557d7895232f
Amreta09 <52856519+Amreta09@users.noreply.github.com>
2019-07-17 19:58:10 +0530

    Update .c

--- .c
+++ .c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,rev=0,c;
+int a,rev=0,c,b;
 scanf("%d",&a);
-while(a!=0)
+  b=a;
+while(b!=0)
 {
-c=a%10;
+c=b%10;
 rev=rev*10+c;
-a=a/10;
+b=b/10;
 }
 if(a==rev)
 {
